ad378348775bf6ed7bfe488c4ddd2170636482a7
Thad House <thhous@microsoft.com>
2021-03-15 14:43:31 -0700

    Initial ETW Reader commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Performance.Toolkit.Engine;
+using Microsoft.Performance.SDK.Extensibility;
+using System;
+using Microsoft.Performance.SDK.Extensibility.SourceParsing;
+using Microsoft.Performance.SDK.Processing;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Diagnostics.Tracing;
+
+namespace EtwReader
+{
+
+    class Program
+    {
+        private static readonly Guid DataGuid = new Guid("ff15e657-4f26-570e-88ab-0796b258d11c");
+
+        static void ParseEvent(TraceEvent data)
+        {
+            if (data.ProviderGuid == DataGuid)
+            {
+                // Parse data for DataGuid here
+                ;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            String etlFile;
+            if (args.Length != 1)
+            {
+                etlFile = @"C:\Users\thhous\Desktop\withbatching\tput.etl";
+            } else
+            {
+                etlFile = args[0];
+            }
+
+
+            using ETWTraceEventSource source = new ETWTraceEventSource(etlFile);
+
+            source.Dynamic.All += ParseEvent;
+            source.Process();
+        }
+    }
+}
